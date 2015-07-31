299d746481ab775d1d8934bdf2907a4bb3e2c9f8
keiwop <jaimepaswanadoo@gmail.com>
2015-08-01 00:33:27 +0200

    Rewrote from scratch the program. Uses now my own version of notebook. Added keybinding. Lots of misc stuff

--- test_gtk3_vte.py
+++ test_gtk3_vte.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 
-from gi.repository import Gtk as gtk, Vte as vte, GLib as glib, Gdk as gdk, Gio as gio
+from gi.repository import Gtk as gtk, Vte as vte, GLib as glib, Gdk as gdk, Gio as gio, Keybinder
+import weakref
 import sys
 import os
 
@@ -10,339 +11,238 @@ default_height = 500
 
 css_file = 'test_vte.css'
 
-#window_opacity = 0.90
-#notebook_opacity = 0.95
-#terminal_opacity = 0.80
-window_opacity = 1
-notebook_opacity = 1
-terminal_opacity = 1
-terminal_scrollback = 9999
-terminal_encoding = "UTF-8"
-terminal_scroll_output = True
-terminal_scroll_key = True
+nb_term = 0
 
-def hex_to_RGBA(hex_color):
-	RGBA_color = gdk.RGBA()
-	RGBA_color.parse(hex_color)
-	RGBA_color.to_string()
-	return RGBA_color
+terminal_list = []
+tab_list = []
 
-c_base03 = hex_to_RGBA("#002B36")
-c_base02 = hex_to_RGBA("#073642")
-c_base01 = hex_to_RGBA("#586e75")
-c_base00 = hex_to_RGBA("#657b83")
-c_base0 = hex_to_RGBA("#839496")
-c_base1 = hex_to_RGBA("#93a1a1")
-c_base2 = hex_to_RGBA("#eee8d5")
-c_base3 = hex_to_RGBA("#fdf6e3")
-c_yellow = hex_to_RGBA("#b58900")
-c_orange = hex_to_RGBA("#cb4b16")
-c_red = hex_to_RGBA("#dc322f")
-c_magenta = hex_to_RGBA("#d33682")
-c_violet = hex_to_RGBA("#6c71c4")
-c_blue = hex_to_RGBA("#268bd2")
-c_cyan = hex_to_RGBA("#2aa198")
-c_green = hex_to_RGBA("#859900")
+class TestMainBox(gtk.Box):
+	
+	def __init__(self):
+		gtk.Box.__init__(self, orientation=gtk.Orientation.VERTICAL)
+		gtk.StyleContext.add_class(self.get_style_context(), "linked")
+		
+		self.create_term_box()
+		self.create_tool_box()
+#		self.term_box.new_term()
+#		self.add_new_term("Terminal")
+		self.active_tab = 0
+		self.add_new_tab("Term")
 
-theme_used = "solarized_dark"
+		
+	def create_term_box(self):
+		self.term_box = TestTermBox(self)
+		self.pack_start(self.term_box, True, True, 0)
+	
+	def create_tool_box(self):
+		self.tool_box = TestToolBox(self)
+		self.pack_end(self.tool_box, False, True, 0)
+	
+	def add_new_tab(self, tab_label):
+		global nb_term
+		self.term_box.add_term()
+		self.tool_box.tab_box.add_tab(tab_label)
+		nb_term = len(terminal_list)
+		print("terminal_list:", terminal_list)
+		print("tab_list:", tab_list)
+		print("Lenght terminal_list:", len(terminal_list))
+		print("Lenght tab_list:", len(tab_list))
+		print("nb_term:", nb_term)
+		self.set_term_active(nb_term)
+		self.show_all()
+			
+	def set_term_active(self, nb_tab):
+		print("a", self.active_tab, "b", nb_tab)
+		if self.active_tab != nb_tab:
+			i = 0
+			for tab in tab_list:
+				tab_list[i].set_name("tab_button")
+				i += 1
+			
+			self.active_tab = nb_tab
+			print("active_tab:", self.active_tab)
+			tab_list[self.active_tab - 1].set_name("active_button")
+			self.active_term = terminal_list[self.active_tab - 1]
+			
+			if nb_term > 1:
+				print("Removing old terminal")
+#				self.term_box.remove(self.active_term)
+				self.term_box.destroy()
+				self.create_term_box()
+#				self.tool_box.tab_box.add_tab(str(self.active_tab))
+#			else:
+			print("New active term:", self.active_term)
+#			self.term_box.add_term(self.active_term)
+			self.term_box.add(self.active_term)
+			self.show_all()
+#			print("FOCUS:",self.term_box.term.has_focus())
+#			self.term_box.grab_focus()
+#			self.set_focus(self.term_box.term)
+#			self.term_box.set_accept_focus(True)
+		
+	
+class TestTermBox(gtk.Box):
+	
+	def __init__(self, parent):
+		gtk.Box.__init__(self, orientation=gtk.Orientation.HORIZONTAL)
+#		gtk.Box.__init__(self)
+		gtk.StyleContext.add_class(self.get_style_context(), "linked")
+		self.set_name("term_box")
+		self.parent = parent
+		
+	def add_term(self):
+		global nb_term, terminal_list
+		nb_term += 1
+		self.term = TestTerminal()
+		self.term.set_hexpand(True)
+#		self.grab_focus()
+		self.term.grab_focus()
+#		self.term.connect("child-exited", self.remove_term)
+		terminal_list.append(self.term)
 
-terminal_palette_solarized_dark = [c_base01, c_red, c_green, c_yellow, c_blue, c_magenta, c_cyan, c_base2, c_base01, c_red, c_green, c_yellow, c_blue, c_magenta, c_cyan, c_base2]
-terminal_palette_solarized_light = [c_base01, c_red, c_green, c_yellow, c_blue, c_magenta, c_cyan, c_base1, c_base01, c_red, c_green, c_yellow, c_blue, c_magenta, c_cyan, c_base1]
+#	def remove_term(self, term, arg):
+#		global nb_term, terminal_list
+#		print(nb_term, "terminal remaining")
+#		print("Removing active terminal")
+#		nb_term -= 1
+##		self.term.destroy()
+#		print("Term list:", terminal_list)
+#		i = 0
+#		for term in terminal_list:
+#			print("term:", term)
+#			if self.term == term:
+#				print("self:", self.term)
+#				break
+#			i += 1
+#		print("i:", i)
+#		terminal_list[i].destroy()
+#		terminal_list.pop(i)
+#		print("Term list:", terminal_list)
+#		if nb_term <= 1: 
+#			self.add_term()
+#			self.show_all()
 
-terminal_palette_size = 8
-#terminal_color_bg = gdk.RGBA(#002B36)
-#terminal_color_fg = gdk.RGBA(1, 0, 0, 1)
-terminal_color_bg_dark = c_base03
-terminal_color_fg_dark = c_base0
-terminal_color_bg_light = c_base3
-terminal_color_fg_light = c_base00
-terminal_color_cursor = gdk.RGBA(0, 0, 1, 1)
-terminal_color_highlight_bg = gdk.RGBA(0, 1, 1, 1)
-terminal_color_highlight_fg = gdk.RGBA(1, 0, 1, 1)
-terminal_bg_image = ""
 
-TOP = gtk.PositionType.TOP
-RIGHT = gtk.PositionType.RIGHT
-BOTTOM = gtk.PositionType.BOTTOM
-LEFT = gtk.PositionType.LEFT
+class TestToolBox(gtk.Box):
+	
+	def __init__(self, parent):
+		gtk.Box.__init__(self, orientation=gtk.Orientation.HORIZONTAL)
+		gtk.StyleContext.add_class(self.get_style_context(), "linked")
+		self.set_name("tool_box")
+		self.parent = parent
+		
+		self.create_tab_box()
+		self.create_shortcut_box()
+		
+	def create_tab_box(self):
+		self.tab_box = TestTabBox(self)
+		self.pack_start(self.tab_box, True, True, 0)
+		
+	def create_shortcut_box(self):
+		self.shortcut_box = TestShortcutBox(self)
+		self.pack_end(self.shortcut_box, False, True, 0)
+	
 
-nb_pages = 1
 
-class HeaderBarWindow(gtk.Window):
+
+class TestTabBox(gtk.Box):
+	
+	def __init__(self, parent): 
+		gtk.Box.__init__(self, orientation=gtk.Orientation.HORIZONTAL)
+		gtk.StyleContext.add_class(self.get_style_context(), "linked")
+		self.set_name("tab_box")
+		self.parent = parent
+		
+#		self.tab_list = []
+		
+	def add_tab(self, tab_label):
+		global tab_list
+		self.button_tab = gtk.Button(tab_label)
+		self.button_tab.connect("clicked", self.on_button_tab_clicked)
+		tab_list.append(self.button_tab)
+		self.add(self.button_tab)
+		
+	def remove_tab(self, tab_nb):
+		self.remove(self.tab_list[tab_nb])
+		
+	def on_button_tab_clicked(self, widget):
+		i = 0
+		for tab in tab_list:
+			if tab == widget:
+				break
+			i += 1
+		self.parent.parent.set_term_active(i + 1)
+
+
+class TestShortcutBox(gtk.Box):
+	
+	def __init__(self, parent): 
+		gtk.Box.__init__(self, orientation=gtk.Orientation.HORIZONTAL)
+		gtk.StyleContext.add_class(self.get_style_context(), "linked")
+		self.set_name("shortcut_box")
+		self.parent = parent
+		
+#		print("Parent:", parent)
+		self.add_shortcuts()
+		
+	def add_shortcuts(self):
+		self.button_new_tab = gtk.Button()
+		icon = gio.ThemedIcon(name="tab-new-symbolic")
+		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
+		self.button_new_tab.add(image)
+		self.button_new_tab.connect("clicked", self.on_button_new_tab_clicked)
+		self.add(self.button_new_tab)
+	
+	def on_button_new_tab_clicked(self, widget):
+		print("New tab")
+		self.parent.parent.add_new_tab(len(terminal_list))
+#		self.parent.parent.set_term_active(len(terminal_list) - 1)
+
+		
+		
+
+class TestHeaderBar(gtk.Window):
 
 	def __init__(self):
-#		window = HeaderBarWindow()
-		gtk.Window.__init__(self, title="TEST")
-#		self.set_border_width(10)
-		self.set_default_size(default_width, default_height)
-
+		gtk.Window.__init__(self, title="test")
+#		self.set_default_size(default_width, default_height)
+#		self.set_accept_focus(True)
 		hb = gtk.HeaderBar()
 		hb.set_show_close_button(True)
-		hb.props.title = "Test VTE 3"
+		hb.props.title = "Test VTE 4"
 		hb.set_name("header_bar")
-		self.set_titlebar(hb)
 
+		box_end = gtk.Box(orientation=gtk.Orientation.HORIZONTAL)
+		gtk.StyleContext.add_class(box_end.get_style_context(), "linked")
+		
 		button_menu = gtk.Button()
 		icon = gio.ThemedIcon(name="open-menu-symbolic")
 		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
 		button_menu.add(image)
 		button_menu.connect("clicked", self.on_button_menu_clicked)
-
-		box = gtk.Box(orientation=gtk.Orientation.HORIZONTAL)
-		gtk.StyleContext.add_class(box.get_style_context(), "linked")
-
-		button_new_tab = gtk.Button()
-		icon = gio.ThemedIcon(name="tab-new-symbolic")
-		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
-		button_new_tab.add(image)
-#		button_new_tab.connect("clicked", self.on_button_new_tab_clicked)
-		box.add(button_new_tab)
-
-		button_stop_process = gtk.Button()
-		icon = gio.ThemedIcon(name="process-stop-symbolic")
-		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
-		button_stop_process.add(image)
-#		button_stop_process.connect("clicked", self.on_button_stop_process_clicked)
-		box.add(button_stop_process)
-
-		hb.pack_start(box)
-		hb.pack_end(button_menu)
 		
-#		self.add(gtk.Box)
-		self.add(TestBox())		
+		box_end.add(button_menu)
+		hb.pack_end(box_end)
 		
-#		self.main_box = TestMainBox()
-#		self.add(self.main_box)
-	
+		self.set_titlebar(hb)
+		self.main_box = TestMainBox()
+		self.add(self.main_box)
+		self.set_focus(self.main_box.term_box.term)
+		
 	def on_button_menu_clicked(self, widget): 
 		print("Menu clicked")
-
-#	def on_button_new_tab_clicked(self, widget): 
-#		print("New tab clicked")
-#		global window
-#		self.notebook.add_page(str(nb_pages))
-#		self.show_all()
-
-#	def on_button_stop_process_clicked(self, widget): 
-#		print("Stop process clicked")
-
-
-class TestBox(gtk.Box):
-	
-	def __init__(self):
-		gtk.Box.__init__(self, orientation=gtk.Orientation.HORIZONTAL)
-		gtk.StyleContext.add_class(self.get_style_context(), "linked")
-		self.set_name("test_box")
-		
-		self.notebook = TestNotebook("Term")
-		self.notebook.add_page(str(nb_pages))
-#		self.notebook.add_page(str(nb_pages))
-		self.notebook.set_opacity(notebook_opacity)
-		
-		self.pack_end(self.notebook, True, True, 0)
-#		print("Notebook:", self.notebook)
-		self.execbox = TestExecBox(self.notebook)
-		self.pack_start(self.execbox, False, True, 0)	
-#		self.show_all()
-
-class TestExecBox(gtk.Box):
-	
-	def __init__(self, notebook):
-		gtk.Box.__init__(self, orientation=gtk.Orientation.VERTICAL)
-		gtk.StyleContext.add_class(self.get_style_context(), "linked")
-		
-#		self.override_background_color(gtk.StateType.NORMAL, gdk.RGBA(1,0,0,1))
-		self.set_name("test_exec_box")
-		self.notebook = notebook
-		print("NOTEBOOK:", self.notebook)
-		
-		button_new_tab = gtk.Button()
-		icon = gio.ThemedIcon(name="tab-new-symbolic")
-		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
-		button_new_tab.add(image)
-		button_new_tab.connect("clicked", self.on_button_new_tab_clicked)
-		button_new_tab.set_name("button_new_tab")
-#		eb = gtk.EventBox()
-#		eb.add(button_new_tab)
-#		button_new_tab.modify_fg(gtk.StateType.NORMAL, gdk.Color(red=65535, green=65535, blue=65535))
-		
-#		color = gdk.color_parse('#234fdb')
-#		button_new_tab.modify_bg(gtk.StateType.PRELIGHT, color)
-
-		
-		self.add(button_new_tab)
-#		self.add(eb)
-		
-		button_quit_terminal = gtk.Button()
-		icon = gio.ThemedIcon(name="edit-delete-symbolic")
-		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
-		button_quit_terminal.add(image)
-		button_quit_terminal.connect("clicked", self.on_button_quit_terminal_clicked)
-		self.add(button_quit_terminal)
-
-		button_stop_process = gtk.Button()
-		icon = gio.ThemedIcon(name="process-stop-symbolic")
-		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
-		button_stop_process.add(image)
-		button_stop_process.connect("clicked", self.on_button_stop_process_clicked)
-		self.add(button_stop_process)
-		
-		button_clear_terminal = gtk.Button()
-		icon = gio.ThemedIcon(name="edit-clear-symbolic")
-		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
-		button_clear_terminal.add(image)
-		button_clear_terminal.connect("clicked", self.on_button_clear_terminal_clicked)
-		self.add(button_clear_terminal)
-		
-		button_fullscreen = gtk.Button()
-		icon = gio.ThemedIcon(name="view-fullscreen-symbolic")
-		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
-		button_fullscreen.add(image)
-		button_fullscreen.connect("clicked", self.on_button_fullscreen_clicked)
-		self.add(button_fullscreen)
-		
-		button_restore_screen = gtk.Button()
-		icon = gio.ThemedIcon(name="view-restore-symbolic")
-		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
-		button_restore_screen.add(image)
-		button_restore_screen.connect("clicked", self.on_button_restore_screen_clicked)
-		self.add(button_restore_screen)
-		
-		button_toggle_titlebar = gtk.Button()
-		icon = gio.ThemedIcon(name="focus-top-bar-symbolic")
-		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
-		button_toggle_titlebar.add(image)
-		button_toggle_titlebar.connect("clicked", self.on_button_toggle_titlebar_clicked)
-		self.add(button_toggle_titlebar)
-		
-		button_quit = gtk.Button()
-		icon = gio.ThemedIcon(name="window-close-symbolic")
-		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
-		button_quit.add(image)
-		button_quit.connect("clicked", self.on_button_quit_clicked)
-		self.pack_end(button_quit, False, True, 0)
-		
-		
-		
-	def on_button_new_tab_clicked(self, widget):
-		print("New tab")
-#		global nb_pages
-		print("NOTEBOOK=:", self.notebook)
-#		nb_pages += 1
-		self.notebook.add_page(str(nb_pages))
+		self.main_box.term_box.remove_term(self.main_box.term_box.term, 0)
 		self.show_all()
-	
-	def on_button_stop_process_clicked(self, widget):
-		print("Stop process")
-	
-	def on_button_quit_clicked(self, widget):
-		print("Quitting application")
-		gtk.main_quit()
-
-	def on_button_clear_terminal_clicked(self, widget):
-		print("Clearing terminal")
-	
-	def on_button_quit_terminal_clicked(self, widget):
-		print("Quitting terminal")
-	
-	def on_button_fullscreen_clicked(self, widget):
-		print("Fullscreen")
-	
-	def on_button_restore_screen_clicked(self, widget):
-		print("Restoring window")
-
-	def on_button_toggle_titlebar_clicked(self, widget):
-		print("Toggling titlebar")
 		
-#class TestMainBox(gtk.Box):
-#	
-#	def __init__(self):
-##		gtk.VBox.__init__(self)
-#		gtk.Box.__init__(self, orientation=gtk.Orientation.VERTICAL)
-#		gtk.StyleContext.add_class(self.get_style_context(), "linked")
-#		
-
-#		terminal_part = gtk.ScrolledWindow()
-#		terminal_part.add(TestTerminal())
-##		pack_start(child, expand=True, fill=True, padding=0)
-#		self.pack_start(terminal_part, True, True, 0)
-
-##		button_new_tab2 = gtk.Button()
-##		icon = gio.ThemedIcon(name="tab-new-symbolic")
-##		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
-##		button_new_tab2.add(image)
-##		button_new_tab2.connect("clicked", self.on_button_new_tab2_clicked)
-#		tab_part = TestTabBox()
-#		self.pack_end(tab_part, False, True, 0)
-
-
-#class TestTabBox(gtk.Box):
-#	
-#	def __init__(self):
-#		gtk.Box.__init__(self, orientation=gtk.Orientation.HORIZONTAL)
-#		gtk.StyleContext.add_class(self.get_style_context(), "linked")
-#		
-#		self.add_tab("Term")
-
-#		button_new_tab = gtk.Button()
-#		icon = gio.ThemedIcon(name="tab-new-symbolic")
-#		image = gtk.Image.new_from_gicon(icon, gtk.IconSize.BUTTON)
-#		button_new_tab.add(image)
-#		button_new_tab.connect("clicked", self.on_button_new_tab_clicked)
-#		self.pack_end(button_new_tab, False, True, 0)		
-#				
-#	def add_tab(self, tab_label):
-#		global nb_pages
-#		nb_pages += 1
-#		button = gtk.Button(tab_label)
-#		self.pack_start(button, False, True, 0)
-#		
-#	def on_button_new_tab_clicked(self, widget): 
-#		print("New tab clicked")
-#		self.add_tab(str(nb_pages))
-#		self.show_all()
 		
-class TestNotebook(gtk.Notebook):
-
-	def __init__(self, page_label):
-		gtk.Notebook.__init__(self)
-		self.set_tab_pos(BOTTOM)
-		self.pages = []
-		self.add_page(page_label)
-		self.set_name("notebook")
-	
-	def add_page(self, page_label):
-		global nb_pages
-		nb_pages += 1
-#		page = TestTerminal()
-		page = gtk.ScrolledWindow()
-		page.add(TestTerminal())
-		self.pages.append(page)
-#		print("PAGES", self.pages)
-		print("NB PAGES: ", nb_pages)
-		self.append_page(page, gtk.Label(page_label))
-		self.set_tab_reorderable(page, True)
-		self.show_all()
-
-	def remove_page(self, a):
-#		print(self)
-		global nb_pages
-		nb_pages -= 1
-		if nb_pages > 1:
-			print("Closing a page")
-			print("Remaining pages:", nb_pages - 1)
-			self.destroy()
-		else:
-			print("Closing application")
-			gtk.main_quit()
 
 
 class TestTerminal(vte.Terminal):
-
+	
 	def __init__(self):
 		vte.Terminal.__init__(self)
 		self.set_name("terminal")
-		self.connect("child-exited", TestNotebook.remove_page)
+#		self.connect("child-exited", TestNotebook.remove_page)
 		self.spawn_sync(
 				vte.PtyFlags.DEFAULT,
 				os.environ['HOME'],
@@ -352,37 +252,16 @@ class TestTerminal(vte.Terminal):
 				None,
 				None,
 				)
-		self.set_encoding(terminal_encoding)
-		self.set_scrollback_lines(terminal_scrollback)
-		self.set_scroll_on_output(terminal_scroll_output) 
-		self.set_scroll_on_keystroke(terminal_scroll_key)
-
-#		self.set_color_background(terminal_color_bg) 
-#		self.set_color_foreground(terminal_color_fg)
-#		self.set_color_cursor(terminal_color_cursor)
-#		self.set_color_highlight(terminal_color_highlight_bg)
-#		self.set_color_highlight_foreground(terminal_color_highlight_fg)
-
-		if theme_used == "solarized_dark":
-			self.set_colors(terminal_color_fg_dark, terminal_color_bg_dark, terminal_palette_solarized_dark)
-		elif theme_used == "solarized_light":
-			self.set_colors(terminal_color_fg_light, terminal_color_bg_light, terminal_palette_solarized_light)
-		
-		if terminal_bg_image != "":
-			self.set_background_image_file(terminal_bg_image)  
-		self.set_opacity(terminal_opacity)
 
 
-
-class MainWindow(HeaderBarWindow):
+class MainWindow(TestHeaderBar):
 	
 	def __init__(self):
-#		window = HeaderBarWindow()
-		HeaderBarWindow.__init__(self)
+		TestHeaderBar.__init__(self)
 		self.connect("delete-event", gtk.main_quit)
-		self.set_opacity(window_opacity)
 		self.set_name("main_window")
-		
+		self.set_default_size(default_width, default_height)
+		self.is_present = True
 		style_provider = gtk.CssProvider()
 
 		css = open((css_file), 'rb') # rb needed for python 3 support
@@ -397,8 +276,27 @@ class MainWindow(HeaderBarWindow):
 		)
 		
 		self.show_all()
+		self.main_box.term_box.term.grab_focus()
+		self.set_focus(self.main_box.term_box.term)
+		
+		keystr = "Menu"
+		Keybinder.init()
+		Keybinder.bind(keystr, self.callback, "keystring %s (user data)" % keystr)
+		print ("Press", keystr, "to handle keybinding and quit")
+	
+	def callback(self, keystr, user_data):
+		print ("Handling", user_data)
+		print ("Event time:", Keybinder.get_current_event_time())
+		if self.is_present:
+			self.iconify()
+			self.is_present = False
+		else:
+			self.present()
+			self.is_present = True
 
 
 if __name__ == "__main__":
 	MainWindow()
 	gtk.main()
+	
+	
